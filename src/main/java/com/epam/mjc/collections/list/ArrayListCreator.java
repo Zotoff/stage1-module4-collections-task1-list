f861277fd36d7f2d3203aeccbc1fe71e5a538893
Pavel Zotov <VTB4069268@region.vtb.ru>
2023-08-03 12:07:11 +0300

    First attempt

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,13 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> arrList = new ArrayList<>();
+        for (int i = 1; i < sourceList.size(); i++) {
+            if ((i + 1) % 3 == 0) {
+                arrList.add(sourceList.get(i));
+                arrList.add(sourceList.get(i));
+            }
+        }
+        return arrList;
     }
 }
